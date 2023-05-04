cfdd30b91abe5eecb7baaae2dc87012abb24505b
GonzaloPulido <gpulido.alumno@iesrafaelalberti.es>
2023-05-04 16:01:38 +0200

    Cambios varios

--- src/components/Galeria.jsx
+++ src/components/Galeria.jsx
@@ -1,99 +1,106 @@
-import React, { Suspense, lazy } from 'react'
-import { useState } from 'react'
-import { useEffect } from 'react'
-import { NavLink } from 'react-router-dom'
-import noimage from '../img/noimage.png'
-import fav from '../img/fav.svg'
-import nofav from '../img/nofav.svg'
-import BotonTop from './BotonTop'
-
+import React, { useState, useEffect } from 'react';
+import { NavLink } from 'react-router-dom';
+import noimage from '../img/noimage.png';
+import fav from '../img/fav.svg';
+import nofav from '../img/nofav.svg';
+import BotonTop from './BotonTop';
 
 const Galeria = ({ url }) => {
-  
-  const [characters, setCharacters] = useState([])
-  const [showLoading, setShowLoading] = useState(false)
-  const [visibleCount, setVisibleCount] = useState(21)
-  const [loading, setLoading] = useState(false)
-  const favorites = localStorage.getItem("favoritos") ? JSON.parse(localStorage.getItem("favoritos")) : []
-  console.log(JSON.parse(localStorage.getItem("favoritos")))
+  const [characters, setCharacters] = useState([]);
+  const [showLoading, setShowLoading] = useState(false);
+  const [visibleCount, setVisibleCount] = useState(21);
+  const [loading, setLoading] = useState(false);
+  const [searchTerm, setSearchTerm] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
+  const favorites = localStorage.getItem('favoritos') ? JSON.parse(localStorage.getItem('favoritos')) : [];
 
-  const eleccionFuncion = (e,personaje) => {
-    if(personaje.favBool){
-      removeFromFavorites(e, personaje)
-    }else{
-      addToFavorites(e,personaje)
+  const eleccionFuncion = (e, personaje) => {
+    if (personaje.favBool) {
+      removeFromFavorites(e, personaje);
+    } else {
+      addToFavorites(e, personaje);
     }
-  }
-
-  const addToFavorites = (e,personaje) => {
-    personaje.favBool = true
-    e.target.src = fav
-    favorites.push(personaje)
-    console.log("Add")
-    console.log(favorites)
-    actualizarFavoritos(favorites)
   };
-  
-  
-  const removeFromFavorites = (e,personaje) => {
-    personaje.favBool = false
-    e.target.src = nofav
-    const posicion = favorites.map((object) => {object.id}).indexOf(personaje.id)
-    favorites.splice(posicion,1)
-    console.log("Remove")
-    console.log(favorites)
-    actualizarFavoritos(favorites)
+
+  const addToFavorites = (e, personaje) => {
+    personaje.favBool = true;
+    e.target.src = fav;
+    favorites.push(personaje);
+    actualizarFavoritos(favorites);
+  };
+
+  const removeFromFavorites = (e, personaje) => {
+    personaje.favBool = false;
+    e.target.src = nofav;
+    const posicion = favorites.map((object) => {
+      object.id;
+    }).indexOf(personaje.id);
+    favorites.splice(posicion, 1);
+    actualizarFavoritos(favorites);
   };
 
   const actualizarFavoritos = (favorites) => {
-    localStorage.setItem("favoritos",JSON.stringify(favorites))
-  }
-  
+    localStorage.setItem('favoritos', JSON.stringify(favorites));
+  };
 
   useEffect(() => {
     const llamarApi = async () => {
-      const respuesta = await fetch(url)
-      const charData = await respuesta.json()
+      const respuesta = await fetch(url);
+      const charData = await respuesta.json();
       if (charData != 'undefined') {
-        setCharacters(charData)
+        setCharacters(charData);
       }
-    }
-    llamarApi()
-  }, [])
+    };
+    llamarApi();
+  }, []);
 
   const handleScroll = () => {
-    const { scrollTop, scrollHeight, clientHeight } = document.documentElement
+    const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
 
     if (scrollTop + clientHeight >= scrollHeight - 5 && !loading) {
-      setShowLoading(true)
-      setLoading(true)
+      setShowLoading(true);
+      setLoading(true);
       setTimeout(() => {
-        setVisibleCount(visibleCount + 21)
-        setLoading(false)
-        setShowLoading(false)
-      }, 800)
+        setVisibleCount(visibleCount + 21);
+        setLoading(false);
+        setShowLoading(false);
+      }, 800);
     }
-  }
+  };
 
   useEffect(() => {
-    window.addEventListener('scroll', handleScroll)
+    window.addEventListener('scroll', handleScroll);
 
-    return () => window.removeEventListener('scroll', handleScroll)
-  }, [visibleCount])
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, [visibleCount]);
+
+  const handleChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  useEffect(() => {
+    const results = characters.filter((char) => char.name.toLowerCase().includes(searchTerm.toLowerCase()));
+    setSearchResults(results);
+  }, [searchTerm, characters]);
+
+  const charactersToDisplay = searchTerm ? searchResults : characters;
 
   return (
     <>
-      {characters.slice(0, visibleCount).map((item) => {
-      item.favorito = nofav
-      item.favBool = false
-      let favLocal = JSON.parse(localStorage.getItem("favoritos"))
-      if(favLocal && favLocal.length > 0) {
-        const favorite = favLocal.find(favorite => favorite.id === item.id)
-        if(favorite) {
-          item.favorito = fav
-          item.favBool = true
+      <div className='search'>
+        <input type='text' placeholder='Buscar personaje...' value={searchTerm} onChange={handleChange} />
+      </div>
+      {charactersToDisplay.slice(0, visibleCount).map((item) => {
+        item.favorito = nofav;
+        item.favBool = false;
+        let favLocal = JSON.parse(localStorage.getItem('favoritos'));
+        if (favLocal && favLocal.length > 0) {
+          const favorite = favLocal.find((favorite) => favorite.id === item.id);
+          if (favorite) {
+            item.favorito = fav;
+            item.favBool = true;
+          }
         }
-      }
       
 
       let {id,name,image,favorito} = item
